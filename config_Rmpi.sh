b0fadd9cb47870ec59e4ae2c352e6a374b375427
Steven C. Gonzalez <stevenconnorg@gmail.com>
2017-11-29 20:06:53 -0600

    Update config_Rmpi.sh

--- config_Rmpi.sh
+++ config_Rmpi.sh
@@ -1,22 +1,10 @@
-module load openmpi
-module load R
+cd ~/sources
+wget https://cran.r-project.org/src/contrib/Rmpi_0.6-6.tar.gz 
+gunzip -c Rmpi_0.6-6.tar.gz | tar xf -
 
-wget wget https //mpi4py.googlecode.com/files/mpi4py-1.3.1.tar.gz
-gunzip -c openmpi-3.0.0.tar.gz | tar xf -
-cd openmpi-3.0.0
-./configure --disable-dlopen --enable-shared --prefix=/~/
-make all install
+Module load R
 
-# http://www.stats.uwo.ca/faculty/yu/Rmpi/install.htm
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/openmpi-3.0.0/lib
-export MPI_ROOT=~/openmpi-3.0.0
+R CMD INSTALL Rmpi_0.6-6.tar.gz --configure-args=--with-mpi=/opt/openmpi
 
-
-R CMD INSTALL --configure-args="--with-Rmpi-type='OPENMPI' " Rmpi_0.6-6.tar.gz
-
-also maybe worked:
-R CMD INSTALL Rmpi_0.6-6.tar.gz --no-test-load
-
-
-check R library paths
-.libPaths()
+R
+library(Rmpi)
