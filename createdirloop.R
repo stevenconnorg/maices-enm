392a3abda66e0acb5571c9bb29f49de6d6c873e6
stevenconnorg <stevenconnorg@gmail.com>
2018-03-12 20:31:52 -0500

    outdated

--- createdirloop.R
+++
@@ -1,7 +0,0 @@
-folders<-as.list(ls(),pattern="dir")
-i<-folders[[3]]
-# tk work on function to create these directories
-for (i in folders[[i]])  { 
-  folder<-get(i)
-  dir.create(folder,recursive=TRUE) 
-} 
